916e9a897fd191049f1aa7403193d97889ef09c7
宋神宗 <i@songshenzong.com>
2019-02-16 11:40:37 +0800

    update version and changelog

--- src/AlibabaCloud.php
+++ src/AlibabaCloud.php
@@ -24,7 +24,7 @@ class AlibabaCloud
     /**
      * Version of the Client
      */
-    const VERSION = '1.0.9';
+    const VERSION = '1.0.12';
 
     /**
      * This static method can directly call the specific service.
