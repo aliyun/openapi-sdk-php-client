a3bd7952beebd1248c495132ffc235c853861c0c
顾咏 <zhijiang.nzj@alibaba-inc.com>
2019-02-21 17:38:57 +0800

    release V1.0.14

--- src/AlibabaCloud.php
+++ src/AlibabaCloud.php
@@ -24,7 +24,7 @@ class AlibabaCloud
     /**
      * Version of the Client
      */
-    const VERSION = '1.0.13';
+    const VERSION = '1.0.14';
 
     /**
      * This static method can directly call the specific service.
