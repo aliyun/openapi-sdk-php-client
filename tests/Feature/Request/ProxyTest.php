8b06a26d1362d844131a4ae7d5cceb9ee3d27c50
顾咏 <zhijiang.nzj@alibaba-inc.com>
2019-03-18 16:14:09 +0800

    proxy tests

---
+++ tests/Feature/Request/ProxyTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace AlibabaCloud\Client\Tests\Feature\Request;
+
+use AlibabaCloud\Client\AlibabaCloud;
+use AlibabaCloud\Client\Exception\ClientException;
+use AlibabaCloud\Client\Exception\ServerException;
+use AlibabaCloud\Client\Tests\Mock\Services\Ecs\DescribeRegionsRequest;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ProxyTest
+ *
+ * @package   AlibabaCloud\Client\Tests\Feature\Request
+ * @coversDefaultClass \AlibabaCloud\Client\Request\RpcRequest
+ */
+class ProxyTest extends TestCase
+{
+    /**
+     * @throws ServerException
+     * @throws ClientException
+     */
+    public function testProxyOk()
+    {
+        // Setup
+        $nameClient      = 'name';
+        $regionId        = \getenv('REGION_ID');
+        $accessKeyId     = \getenv('ACCESS_KEY_ID');
+        $accessKeySecret = \getenv('ACCESS_KEY_SECRET');
+
+        // Test
+        AlibabaCloud::accessKeyClient($accessKeyId, $accessKeySecret)
+                    ->regionId($regionId)
+                    ->name($nameClient);
+
+        // Assert
+
+        $result = (new DescribeRegionsRequest())->client($nameClient)
+                                                ->connectTimeout(25)
+                                                ->timeout(30)
+                                                ->proxy([
+                                                            'http' => 'http://localhost:8989',
+                                                        ])
+                                                ->request();
+
+        $this->assertNotNull($result->RequestId);
+        $this->assertNotNull($result->Regions->Region[0]->LocalName);
+        $this->assertNotNull($result->Regions->Region[0]->RegionId);
+    }
+
+    /**
+     * @expectedException \AlibabaCloud\Client\Exception\ClientException
+     * @expectedExceptionMessageRegExp /Failed to connect to 0.0.0.0 port 55657/
+     * @throws ClientException
+     * @throws ServerException
+     */
+    public function testProxyNotSet()
+    {
+        // Setup
+        $nameClient      = 'name';
+        $regionId        = \getenv('REGION_ID');
+        $accessKeyId     = \getenv('ACCESS_KEY_ID');
+        $accessKeySecret = \getenv('ACCESS_KEY_SECRET');
+
+        // Test
+        AlibabaCloud::accessKeyClient($accessKeyId, $accessKeySecret)
+                    ->regionId($regionId)
+                    ->name($nameClient);
+
+        // Assert
+
+        (new DescribeRegionsRequest())->client($nameClient)
+                                      ->connectTimeout(1)
+                                      ->timeout(2)
+                                      ->proxy([
+                                                  'http' => 'http://localhost:55657',
+                                              ])
+                                      ->request();
+    }
+
+}
