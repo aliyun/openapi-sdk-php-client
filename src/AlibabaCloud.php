176089e262ad75d39e6b343522d9bf997c0964a1
songshenzong <i@songshenzong.com>
2019-05-10 10:22:18 +0800

    Improved `Result` object

--- src/AlibabaCloud.php
+++ src/AlibabaCloud.php
@@ -30,7 +30,7 @@ class AlibabaCloud
     /**
      * Version of the Client
      */
-    const VERSION = '1.5.1';
+    const VERSION = '1.5.2';
 
     /**
      * This static method can directly call the specific service.
