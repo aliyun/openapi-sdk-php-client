1782a72f0b14cd71cdf618abc77b1b41fa172234
宋神宗 <zhijiang.nzj@alibaba-inc.com>
2020-07-04 16:02:21 +0800

    - Fixed ROA signature. (#220)

--- src/AlibabaCloud.php
+++ src/AlibabaCloud.php
@@ -30,7 +30,7 @@ class AlibabaCloud
     /**
      * Version of the Client
      */
-    const VERSION = '1.5.24';
+    const VERSION = '1.5.25';
 
     /**
      * This static method can directly call the specific service.
